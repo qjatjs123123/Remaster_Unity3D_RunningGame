f5e384b9836f9e9537a289e0c386520fe4896ae0
Hong Boem Sun <qjatjs123123@naver.com>
2022-01-22 22:47:29 +0900

    라운드2 트랩0 구현

---
+++ Assets/Scripts/Round2_Script/SwingScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingScript : MonoBehaviour
+{
+    Vector3 pos; //현재위치
+    public float delta = 2.0f; // 좌(우)로 이동가능한 (x)최대값
+    public float speed = 3.0f; // 이동속도
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = pos;
+
+        v.x += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
