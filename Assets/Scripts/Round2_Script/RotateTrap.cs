3b56d4be1c1358b6160cf3554bf482c7282bf195
Hong Boem Sun <qjatjs123123@naver.com>
2022-01-28 23:51:06 +0900

    트랩 8 구현 맵 디자인

--- Assets/Scripts/Round2_Script/RotateTrap.cs
+++ Assets/Scripts/Round2_Script/RotateTrap.cs
@@ -6,6 +6,7 @@ public class RotateTrap : MonoBehaviour
 {
     public float rotSpeed = 25f;
     public string direction = "x";
+    public int changeRotate = 1;
     void Update()
     {
         self_Rotation();
@@ -13,11 +14,13 @@ public class RotateTrap : MonoBehaviour
 
     void self_Rotation()
     {
+        
+
         if (direction == "x")
-            transform.Rotate(new Vector3(rotSpeed * Time.deltaTime, 0, 0));
+            transform.Rotate(new Vector3(rotSpeed * Time.deltaTime* changeRotate, 0, 0));
         else if (direction == "y")
-            transform.Rotate(new Vector3(0, rotSpeed * Time.deltaTime, 0));
+            transform.Rotate(new Vector3(0, rotSpeed * Time.deltaTime* changeRotate, 0));
         else if (direction == "z")
-            transform.Rotate(new Vector3(0, 0, rotSpeed * Time.deltaTime));
+            transform.Rotate(new Vector3(0, 0, rotSpeed * Time.deltaTime* changeRotate));
     }
 }
